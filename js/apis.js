760822959672295455d6588007a37cf4f8bd1ab0
Dony Cisneros <dony.cisne@gmail.com>
2018-01-03 23:11:41 -0300

    Initial commit

---
+++ js/apis.js
@@ -0,0 +1,16 @@
+var mapa // Mapa que vamos a modificar
+
+/* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
+
+// Inicializa el mapa con un valor de zoom y una locación en el medio
+function inicializarMapa () {
+    /* Modificá la variable mapa con el constructor Map().
+    Tendrás que asignarle un valor de zoom y
+    un centro igual a la variable posicionCentral. */
+
+  geocodificadorModulo.inicializar()
+  marcadorModulo.inicializar()
+  direccionesModulo.inicializar()
+  lugaresModulo.inicializar()
+  streetViewModulo.inicializar()
+}
---
+++ js/streetView.js
@@ -0,0 +1,21 @@
+streetViewModulo = (function () {
+  var paronama // 'Visor' de StreetView
+
+  function inicializar () {
+        /* Completar la función inicializar()  que crea un panorama
+        en una posición y lo muestra en la página. */
+
+  }
+
+    // Actualiza la ubicación del Panorama
+  function fijarStreetView (ubicacion) {
+        /* Completar la función fijarStreetView que actualiza la posición
+         de la variable panorama y cambia el mapa de modo tal que se vea
+         el streetView de la posición actual. */
+  }
+
+  return {
+    inicializar,
+    fijarStreetView
+  }
+})()
