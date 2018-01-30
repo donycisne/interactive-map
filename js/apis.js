e62a05102348000b5d2044962420045f8f7503e7
Dony Cisneros <dony.cisne@gmail.com>
2018-01-29 23:24:45 -0300

    Show map starting

--- js/apis.js
+++ js/apis.js
@@ -1,13 +1,16 @@
-var mapa // Mapa que vamos a modificar
+var mapa;
 
-/* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
+var posicionCentral = {
+  lat: -34.603739,
+  lng: -58.38157
+};
 
-// Inicializa el mapa con un valor de zoom y una locación en el medio
+// Iniciando el mapa con zoom y la locación en el centro
 function inicializarMapa () {
-    /* Modificá la variable mapa con el constructor Map().
-    Tendrás que asignarle un valor de zoom y
-    un centro igual a la variable posicionCentral. */
-
+  mapa = new google.maps.Map(document.getElementById('map'), {
+    center: posicionCentral,
+    zoom: 13
+  });
   geocodificadorModulo.inicializar()
   marcadorModulo.inicializar()
   direccionesModulo.inicializar()
